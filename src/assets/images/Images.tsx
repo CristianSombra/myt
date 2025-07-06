556e44f056a20ca83a01d3e78cd6e874fd056aba
Cristian Sombra <cristiansombra87@gmail.com>
2025-07-06 16:36:09 -0300

    feat: se actualiza sección About

--- src/assets/images/Images.tsx
+++ src/assets/images/Images.tsx
@@ -8,3 +8,6 @@ export const imageHome ="https://res.cloudinary.com/dk7b7yi87/image/upload/v1751
 export const imageProducts1 = "https://res.cloudinary.com/dk7b7yi87/image/upload/v1751811169/sueteres_1_trlvqu.png";
 export const imageProducts2 = "https://res.cloudinary.com/dk7b7yi87/image/upload/v1751811137/Camisetas_1_andxw3.png";
 export const imageProducts3 = "https://res.cloudinary.com/dk7b7yi87/image/upload/v1751811168/Chaquetas_ok_1_xatimd.png";
+
+//About
+export const imageAbout1 = "https://res.cloudinary.com/dk7b7yi87/image/upload/v1751830338/about1_njcqj0.png";
--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import type {AnimateProps} from "../../components";
+import { imageAbout1 } from "../../assets/images/Images";
 
 const About: React.FC<AnimateProps> = ({animate}) => {
     return(
@@ -10,19 +11,18 @@ const About: React.FC<AnimateProps> = ({animate}) => {
         id="about"
         className="about-container my-5"
         >
-            <div className="row text-center">
-                <div className="col">
-                    <h1>Nosotros</h1>
+           <div className="row d-flex justify-content-center">
+                <div className="col-12 text-center mt-5">
+                    <h1>Sobre Nosotros</h1>
+                </div>
+                 <div className="col-11 col-md-8 col-lg-7 text-center my-4">
+                    <p>En M&T te ofrecemos las últimas tendencias en ropa para adolescentes. Estilo, comodidad y personalidad en cada prenda. Descubrí nuestra colección y encontrá el outfit que mejor te representa.</p>
                 </div>
             </div>
             <div className="row d-flex justify-content-center">
-                {/* <div className="col-10 col-md-4">
-                    <img src={teamWork} className="img-fluid" alt="..."></img>
-                </div> */}
-                <div className="col-10 col-md-4 text-center">
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia repellendus eveniet, quam laudantium amet molestiae possimus minus. Magnam, tempora dolore!
-                    </p>
-                </div>
+                <div className="col-10 col-md-6 col-lg-5 text-center my-5">
+                    <img src={imageAbout1} className="img-fluid rounded-4 shadow" alt="..."></img>
+                </div>           
             </div>
         </motion.section>
     )
