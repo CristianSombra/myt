ddba04317ce41e4673f7cec7a038c4d07ed70015
Cristian Sombra <cristiansombra87@gmail.com>
2025-07-12 14:01:28 -0300

    feat: actualización estilos home, products

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -11,32 +11,32 @@ const Home: React.FC<AnimateProps> = ({animate}) => {
         initial={{ opacity: 0}}
         animate= { animate ? { opacity: 1 } : { opacity: 0}}
         id="home"
-        className="home-container"
+        className="container home-container"
         >
             <div className="row d-flex justify-content-center mt-2 my-md-5">
-                <div className="col-12 col-md-5">
+                <div className="col-12 col-md-6">
                     <div className="row text-center text-md-start">
                         <div className="col-12">
                             <h1 className="mb-0">Moda para</h1>
                             <h1>Adolescentes</h1>
                         </div>
-                        <div className="col-12 col-md-11 col-lg-9 my-3">
+                        <div className="col-12 col-md-11 col-lg-10 my-3">
                             <p>En M&T te ofrecemos las últimas tendencias en ropa para adolescentes. Estilo, comodidad y personalidad en cada prenda. Descubrí nuestra colección y encontrá el outfit que mejor te representa.</p>
                         </div>
-                        <div className="col-12 d-flex justify-content-md-start d-md-block d-none">
-                            <button className="btn btn-dark my-2">
+                        <div className="col-12 d-flex justify-content-md-start d-lg-block d-none">
+                            <a href="#products" className="btn btn-dark">
                                 Ver Productos
-                            </button>
+                            </a>
                         </div>
                     </div>
                 </div>
-                <div className="col-10 col-md-4 col-lg-3 mb-1">
+                <div className="col-10 col-md-4 col-lg-4 mb-1">
                     <img src={imageHome} className="img-fluid rounded-4 shadow" alt="..."></img>
                 </div>
                 <div className="d-flex justify-content-center my-4 d-block d-md-none">
-                    <button className="btn btn-dark">
-                        Ver Productos
-                    </button>
+                    <a href="#products" className="btn btn-dark">
+                      Ver Productos
+                    </a>
                 </div>
             </div>
         </motion.section>
--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import type {AnimateProps} from "../../components";
-import { imageProducts1, imageProducts2, imageProducts3 } from "../../assets/images/Images";
+import { imageProducts1, imageProducts2, imageProducts3, imageProducts4 } from "../../assets/images/Images";
 
 const Services: React.FC<AnimateProps> = ({animate}) => {
     return(
@@ -9,33 +9,37 @@ const Services: React.FC<AnimateProps> = ({animate}) => {
         initial={{ opacity: 0}}
         animate= { animate ? { opacity: 1 } : { opacity: 0}}
         id="products"
-        className="products-container my-5"
+        className="container products-container my-5"
         >
             <div className="row d-flex justify-content-center">
                 <div className="col-12 text-center mt-4">
                     <h1>Nuestros Productos</h1>
                 </div>
-                 <div className="col-11 col-md-6 text-center my-4">
+                 <div className="col-12 col-md-11 col-lg-9 text-center my-4">
                     <p>En M&T te ofrecemos las últimas tendencias en ropa para adolescentes. Estilo, comodidad y personalidad en cada prenda. Descubrí nuestra colección y encontrá el outfit que mejor te representa.</p>
                 </div>
             </div>
-            <div className="row d-flex justify-content-center align-items-center">
-                <div className="col-10 col-md-3 col-lg-2 text-center">
-                    <img src={imageProducts1} className="img-fluid rounded-4" alt="..."></img>
-                    <h3 className="my-3">Suéteres</h3>
+            <div className="row d-flex justify-content-center">
+                <div className="col-10 col-md-5 col-lg-3 text-center">
+                    <img src={imageProducts1} className="img-fluid rounded-4 shadow" alt="..."></img>
+                    <h4 className="my-3">Deportivo</h4>
                     <p>Comodidad y estilo para cualquier ocasión.</p>
                 </div>
-                <div className="col-10 col-md-3 col-lg-2 mx-md-4 mx-lg-5 my-5 text-center">
-                    <img src={imageProducts2} className="img-fluid rounded-4" alt="..."></img>
-                    <h3 className="my-3">Camisetas</h3>
+                <div className="col-10 col-md-5 col-lg-3 text-center">
+                    <img src={imageProducts2} className="img-fluid rounded-4 shadow" alt="..."></img>
+                    <h4 className="my-3">Oversize</h4>
                     <p>Diseños versátiles para tu día a día.</p>
                 </div>
-                <div className="col-10 col-md-3 col-lg-2 text-center">
-                    <img src={imageProducts3} className="img-fluid rounded-4" alt="..."></img>
-                    <h3 className="my-3">Chaquetas</h3>
+                <div className="col-10 col-md-5 col-lg-3 text-center">
+                    <img src={imageProducts3} className="img-fluid rounded-4 shadow" alt="..."></img>
+                    <h4 className="my-3">Estampados</h4>
                     <p>Protección y tendencia en cada detalle.</p>
                 </div>
-               
+                <div className="col-10 col-md-5 col-lg-3 text-center">
+                    <img src={imageProducts4} className="img-fluid rounded-4 shadow" alt="..."></img>
+                    <h4 className="my-3">Nuestros diseños</h4>
+                    <p>Protección y tendencia en cada detalle.</p>
+                </div>
             </div>
         </motion.section>
     )
