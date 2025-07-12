6dd72a3c0bc5f68246aeb7b32d49d90b42edff97
Cristian Sombra <cristiansombra87@gmail.com>
2025-07-12 11:22:03 -0300

    feat: actualizacion Navbar

--- src/layouts/navbar/NavBar.tsx
+++ src/layouts/navbar/NavBar.tsx
@@ -12,7 +12,7 @@ const Navbar: React.FC = () => {
     <nav className="navbar navbar-expand-lg navbar-light bg-white">
       <div className="container-fluid px-lg-5">
         <a className="navbar-brand" href="#home" onClick={closeNavbar}>
-          <img src={logoNav} width={100} alt="M&T Tienda de ropa" />
+          <img src={logoNav} width={90} alt="M&T Tienda de ropa" />
         </a>
         <button
           className="navbar-toggler"
@@ -29,19 +29,19 @@ const Navbar: React.FC = () => {
           className={`collapse navbar-collapse${expanded ? ' show' : ''}`}
           id="navbarSupportedContent"
         >
-          <ul className="navbar-nav mx-auto mb-2 mb-lg-0 gap-lg-5 text-center">
+          <ul className="navbar-nav mx-auto mb-2 mb-lg-0 gap-lg-4 text-center">
             <li className="nav-item">
-              <a className="nav-link fs-3" href="#products" onClick={closeNavbar}>
+              <a className="nav-link fs-4" href="#products" onClick={closeNavbar}>
                 Productos
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link fs-3" href="#about" onClick={closeNavbar}>
+              <a className="nav-link fs-4" href="#about" onClick={closeNavbar}>
                 Nosotros
               </a>
             </li>
-            <li className="nav-item">
-              <a className="nav-link fs-3" href="#contact" onClick={closeNavbar}>
+            <li className="nav-item me-0 me-lg-5">
+              <a className="nav-link fs-4" href="#contact" onClick={closeNavbar}>
                 Contacto
               </a>
             </li>
@@ -50,9 +50,9 @@ const Navbar: React.FC = () => {
             href="https://wa.me/5492964694981"
             target="_blank"
             rel="noopener noreferrer"
-            className="social-link text-success d-lg-block d-none"
+            className="social-link text-success d-lg-block d-none ms-4"
           >
-            <FaWhatsapp size={50} />
+            <FaWhatsapp size={40} />
           </a>
         </div>
       </div>
