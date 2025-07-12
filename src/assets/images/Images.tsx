d9241261828643da2af36891fb07b2d8eca13d50
Cristian Sombra <cristiansombra87@gmail.com>
2025-07-12 11:10:45 -0300

    feat: actualizacion de URLs de imágenes para las secciones

--- src/assets/images/Images.tsx
+++ src/assets/images/Images.tsx
@@ -2,15 +2,16 @@
 export const logoNav = "https://res.cloudinary.com/dk7b7yi87/image/upload/v1751743761/M_T_-_Without_Background_1_e7la8y.png";
 
 //Home
-export const imageHome ="https://res.cloudinary.com/dk7b7yi87/image/upload/v1751810939/opcion_2_xyeeul.png";
+export const imageHome ="https://res.cloudinary.com/dk7b7yi87/image/upload/v1752328772/opcion_2_xyeeul_bg9lae.webp";
 
 //Products
-export const imageProducts1 = "https://res.cloudinary.com/dk7b7yi87/image/upload/v1751811169/sueteres_1_trlvqu.png";
-export const imageProducts2 = "https://res.cloudinary.com/dk7b7yi87/image/upload/v1751811137/Camisetas_1_andxw3.png";
-export const imageProducts3 = "https://res.cloudinary.com/dk7b7yi87/image/upload/v1751811168/Chaquetas_ok_1_xatimd.png";
+export const imageProducts1 = "https://res.cloudinary.com/dk7b7yi87/image/upload/v1752328829/Buzo_ejemplo_cyjbe2.webp";
+export const imageProducts2 = "https://res.cloudinary.com/dk7b7yi87/image/upload/v1752328838/mujer_1_aww6pf.webp";
+export const imageProducts3 = "https://res.cloudinary.com/dk7b7yi87/image/upload/v1752328842/estampado_para_ejemplo_sveqcm.webp";
+export const imageProducts4 = "https://res.cloudinary.com/dk7b7yi87/image/upload/v1752328831/mujer_2_qjkdi3.webp";
 
 //About
-export const imageAbout1 = "https://res.cloudinary.com/dk7b7yi87/image/upload/v1751830338/about1_njcqj0.png";
+export const imageAbout1 = "https://res.cloudinary.com/dk7b7yi87/image/upload/v1752329007/about1_njcqj0_fnvvmt.webp";
 
 //Contact
-export const imageContact1 = "https://res.cloudinary.com/dk7b7yi87/image/upload/v1751836620/contact_a3nci3.png";
+export const imageContact1 = "https://res.cloudinary.com/dk7b7yi87/image/upload/v1752328947/contact_a3nci3_uavecl.webp";
