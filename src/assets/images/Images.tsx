f701f52249080f1e847997892af6b62ab8409a93
Cristian Sombra <cristiansombra87@gmail.com>
2025-07-13 17:40:40 -0300

    feat: actualización firstsection de productos

--- src/assets/images/Images.tsx
+++ src/assets/images/Images.tsx
@@ -10,6 +10,35 @@ export const imageProducts2 = "https://res.cloudinary.com/dk7b7yi87/image/upload
 export const imageProducts3 = "https://res.cloudinary.com/dk7b7yi87/image/upload/v1752328842/estampado_para_ejemplo_sveqcm.webp";
 export const imageProducts4 = "https://res.cloudinary.com/dk7b7yi87/image/upload/v1752328831/mujer_2_qjkdi3.webp";
 
+export const firstSectionImages = [
+  {
+    src: "https://res.cloudinary.com/dk7b7yi87/image/upload/v1752328827/adolescente_joven_1_xoa4bg.webp",
+    title: "Prenda 1",
+    description: "Descripción breve de la prenda."
+  },
+  {
+    src: "https://res.cloudinary.com/dk7b7yi87/image/upload/v1752328828/adolescente_joven_2_fg4363.webp",
+    title: "Prenda 2",
+    description: "Descripción breve de la prenda."
+  },
+  {
+    src: "https://res.cloudinary.com/dk7b7yi87/image/upload/v1752328829/adolescente_mujer_grande_1_qkgc3h.webp",
+    title: "Prenda 3",
+    description: "Descripción breve de la prenda."
+  },
+  {
+    src: "https://res.cloudinary.com/dk7b7yi87/image/upload/v1752328828/adolescente_joven_4_y24mbl.webp",
+    title: "Prenda 4",
+    description: "Descripción breve de la prenda."
+  },
+  {
+    src: "https://res.cloudinary.com/dk7b7yi87/image/upload/v1752328827/adolescente_joven_1_xoa4bg.webp",
+    title: "Prenda 5",
+    description: "Descripción breve de la prenda."
+  }
+];
+
+
 //About
 export const imageAbout1 = "https://res.cloudinary.com/dk7b7yi87/image/upload/v1752329007/about1_njcqj0_fnvvmt.webp";
 
--- src/pages/products/FirstSection.tsx
+++ src/pages/products/FirstSection.tsx
@@ -1,26 +1,26 @@
 import React from "react";
-import { imageProducts1 } from "../../assets/images/Images";
+import { firstSectionImages } from "../../assets/images/Images";
 
 const FirstSectionProducts: React.FC = () => {
 
   return (
     <div className="container py-5">
-      
       <div className="row">
-        <div className="col-md-6">
-          <div className="card mb-4">
-            <img
-              src={imageProducts1}
-              className="card-img-top"
-              alt="Prenda 1"
-            />
-            <div className="card-body">
-              <h5 className="card-title">Prenda 1</h5>
-              <p className="card-text">Descripción breve de la prenda.</p>
+        {firstSectionImages.map((item, index) => (
+          <div className="col-md-6 col-lg-4" key={index}>
+            <div className="card mb-4">
+              <img
+                src={item.src}
+                className="card-img-top"
+                alt={item.title}
+              />
+              <div className="card-body">
+                <h5 className="card-title">{item.title}</h5>
+                <p className="card-text">{item.description}</p>
+              </div>
             </div>
           </div>
-        </div>
-        
+        ))}
       </div>
     </div>
   );
