c83d020ccd563ed97adffa233bbea95341891344
Cristian Sombra <cristiansombra87@gmail.com>
2025-07-21 17:20:08 -0300

    feat: estilos footer

--- src/layouts/footer/Footer.tsx
+++ src/layouts/footer/Footer.tsx
@@ -8,24 +8,28 @@ const Footer: React.FC = () => {
     <div className="container-fluid">
 
       <footer className="bg-white text-center py-4">
-        <hr className="border border-secondary opacity-10 mb-5"/>
-        <div className="mb-3">
+        <hr className="border border-secondary opacity-10 mb-3 mb-md-4"/>
+        <div className="row d-flex justify-content-center my-3 mt-4 mx-md-auto">
+          <div className="col-3 col-md-1 mb-3">
           <a
             href="https://www.facebook.com/cristiansombra87"
             target="_blank"
             rel="noopener noreferrer"
-            className="social-icon facebook me-3"
+            className="social-icon facebook"
             >
-            <FontAwesomeIcon icon={faFacebook} size="2x" />
+            <FontAwesomeIcon icon={faFacebook}  style={{ fontSize: '40px' }}  />
           </a>
-          <a
-            href="https://www.instagram.com/cristiansombra87/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="social-icon instagram"
-            >
-            <FontAwesomeIcon icon={faInstagram} size="2x" />
-          </a>
+          </div>
+          <div className="col-3 col-md-1">
+            <a
+              href="https://www.instagram.com/cristiansombra87/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="social-icon instagram"
+              >
+              <FontAwesomeIcon icon={faInstagram} style={{ fontSize: '40px' }}  />
+            </a>
+          </div>
         </div>
         <div className="small text-muted">
           M&T - Todos los derechos reservados © 2025
